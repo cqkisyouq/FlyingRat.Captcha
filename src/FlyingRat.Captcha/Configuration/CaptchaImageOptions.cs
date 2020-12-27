88461afce559f81b8e466af70cbd62304c164f8a
long <longtian11023@qq.com>
2020-12-27 22:43:25 +0800

    删除文件名，大小不统一

--- src/FlyingRat.Captcha/Configuration/CaptchaImageOptions.cs
+++ src/FlyingRat.Captcha/Configuration/CaptchaImageOptions.cs
@@ -8,8 +8,8 @@ namespace FlyingRat.Captcha.Configuration
     {
         public string Root { get; set; } = "wwwroot/flyingrat";
         public string Background_Path { get; set; } = "image/background";
-        public string Slider_Path { get; set; } = "Image/sliders";
-        public string Lump_Path { get; set; } = "Image/lump";
+        public string Slider_Path { get; set; } = "image/sliders";
+        public string Lump_Path { get; set; } = "image/lump";
         public string Alpha_Name { get; set; }= "alpha.png";
         public string Border_Name { get; set; } = "border.png";
         public string Image_Name { get; set; } = "image.png";
